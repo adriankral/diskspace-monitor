306caec6970775571fca97619a9d34d4311f2a6c
Adrian Rudolf Kral <adriankral@gmail.com>
2014-04-21 16:11:20 +0200

    Initial commit.
    Untested version without sync check support.

---
+++ diskspace-monitor.js
@@ -0,0 +1,100 @@
+var EventEmitter = require('events').EventEmitter;
+var diskspace = require('diskspace');
+
+var ERR_NO_PATH = "no path provided";
+var DEF_UNIT = "MB";
+
+var attributes = {
+  timeout: setTimeout,
+  lowSpaceThres: setLowSpaceThres,
+  highSpaceThres: setHighSpaceThres
+};
+
+var spaceFactor = {
+  GB: 1024 * 1024 * 1024,
+  MB: 1024 * 1024,
+  KB: 1024,
+  B: 1
+}
+
+function DiskSpaceMonitor(_path)
+{
+  if(!path)
+    throw new Error(ERR_NO_PATH);
+  this.path = _path;
+}
+
+function setQueryTimeout(_timeout)
+{
+  this.queryTimeout = _timeout;
+  return true;
+}
+
+function setLowSpaceThres(_threshold, _unit)
+{
+  if(_unit && !spaceFactor[_unit])
+    return false;
+  if(!this.lowSpaceUnit)
+    this.lowSpaceUnit = _unit ? _unit : DEF_UNIT;
+  this.lowSpaceThres = _threshold * spaceFactor[this.lowSpaceUnit];
+  return true;
+}
+
+function setHighSpaceThres(_threshold, _unit)
+{
+  if(_unit && !spaceFactor[_unit])
+    return false;
+  if(!this.highSpaceUnit)
+    this.highSpaceUnit = _unit ? _unit : DEF_UNIT;
+  this.highSpaceThres = _threshold * spaceFactor[this.highSpaceUnit];
+  return true;
+}
+
+function activate(_args)
+{
+  for(_arg in _args)
+  {
+    if(!attributes[arg])
+      continue;
+    if(!attributes[arg].apply(this, _args[_arg]))
+      return false;
+  }
+  if(!this.queryTimeout)
+    return false;
+  this.diskSpaceQuery =
+    setTimeout(makeDiskSpaceQuery.apply(this), this.queryTimeout);
+  return true;
+}
+
+function deactivate()
+{
+  clearTimeout(this.diskSpaceQuery);
+  return true;
+}
+
+function makeDiskSpaceQuery()
+{
+  diskspace.check(this.path, function(total, free, status){
+    if(status !== 'READY')
+      this.emit('error', status);
+    else
+    {
+      if(this.lowSpaceThres && free < this.lowSpaceThres)
+        this.emit('lowDiskSpace', free / spaceFactor[this.lowSpaceUnit]);
+      else if(this.highSpaceThres && free > this.highSpaceThres)
+        this.emit('highDiskSpace', free / spaceFactor[this.highSpaceUnit]);
+    }
+  });
+  return true;
+}
+
+DiskSpaceMonitor.prototype = new EventEmitter();
+DiskSpaceMonitor.prototype.DiskSpaceMonitor = DiskSpaceMonitor;
+DiskSpaceMonitor.prototype.setQueryTimeout = setQueryTimeout;
+DiskSpaceMonitor.prototype.setLowSpaceThres = setLowSpaceThres;
+DiskSpaceMonitor.prototype.setHighSpaceThres = setHighSpaceThres;
+DiskSpaceMonitor.prototype.activate = activate;
+DiskSpaceMonitor.prototype.deactivate = deactivate;
+DiskSpaceMonitor.prototype.makeDiskSpaceQuery = makeDiskSpaceQuery;
+
+exports.DiskSpaceMonitor = DiskSpaceMonitor;
